26047f87b02596b9e9a44e82c19d84a955f23947
Yodusss <wathongmm.21@ichat.sp.edu.sg>
2022-10-27 15:17:07 +0800

    d

---
+++ bmi.py
@@ -0,0 +1,5 @@
+def calculate_bmi(height, weight):
+    print("Height = " + height)
+    print("Weight = " + weight)
+
+
---
+++ ex1a.py
@@ -0,0 +1,3 @@
+def calculate_bmi(height, weight):
+    print("Height = " + height)
+    print("Weight = " + weight)
